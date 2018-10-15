d4bf482be60e9da10c39f3946753f18dd8188a13
Suneshchekkiyil <sunesh@kirana11.com>
2018-10-15 18:44:27 +0530

    services/src/test/java/it/fds/taskmanager/TaskServiceJPATest.java

--- services/src/test/java/it/fds/taskmanager/TaskServiceJPATest.java
+++ services/src/test/java/it/fds/taskmanager/TaskServiceJPATest.java
@@ -1,5 +1,6 @@
 package it.fds.taskmanager;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.junit.Assert;
@@ -39,7 +40,72 @@ public class TaskServiceJPATest extends Assert{
         assertEquals("Test task1", tOut.getTitle());
         List<TaskDTO> list = taskService.showList();
         assertEquals(1, list.size());
+        
     }
+    
+    @Test
+    public void readUpdateOnDB() {
+        TaskDTO t = new TaskDTO();
+        t.setTitle("Check the update method");
+        t.setPriority("5");
+        t.setDescription("Updating the priority of a task");
+        t.setStatus(TaskState.NEW.toString().toUpperCase());
+        TaskDTO t1 = taskService.saveTask(t);
+        TaskDTO t2 = taskService.updateTask(t1);
+        TaskDTO tOut = taskService.findOne(t2.getUuid());
+        assertNotEquals("null", tOut.getUpdatedat());
+        List<TaskDTO> list = taskService.showList();
+        assertEquals(1, list.size());
+    }
+    
+   @Test
+    public void readStatusPostponedOnDB() {
+        TaskDTO t = new TaskDTO();
+        t.setTitle("Check postponed status method");
+        t.setStatus(TaskState.NEW.toString().toUpperCase());
+        TaskDTO t1 = taskService.saveTask(t);
+        TaskDTO tOut = taskService.findOne(t1.getUuid());
+        Boolean t2 = taskService.postponeTask(tOut.getUuid(), 100);
+        TaskDTO tOut1 = taskService.findOne(tOut.getUuid());
+        assertEquals("Check postponed status method", tOut.getTitle());
+        List<TaskDTO> list = taskService.showList();
+        assertEquals(1, list.size());
+        assert (tOut1.getStatus()==TaskState.POSTPONED.toString());
+        
+    }
+    
+   @Test
+    public void readStatusResolvedOnDB(){
+       TaskDTO t = new TaskDTO();
+       t.setTitle("Check Resolved status method");
+       t.setStatus(TaskState.NEW.toString().toUpperCase());
+       TaskDTO t1 = taskService.saveTask(t);
+       TaskDTO tOut = taskService.findOne(t1.getUuid());
+       Boolean t2 = taskService.resolveTask(tOut.getUuid());
+       TaskDTO t3 = taskService.findOne(tOut.getUuid());
+       assertEquals("Check Resolved status method", tOut.getTitle());
+       assert (t3.getStatus()==TaskState.RESOLVED.toString());
+       List<TaskDTO> list = taskService.showList();
+       assertEquals(1, list.size());
+   }
+   
+    @Test
+    public void readUnmarkedPostponeOnDB() {
+        TaskDTO t = new TaskDTO();
+        t.setTitle("Check unmarked postponed status method");
+        t.setStatus(TaskState.POSTPONED.toString().toUpperCase());
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MINUTE, 100);
+        t.setPostponedat(c);
+        TaskDTO t1 = taskService.saveTask(t);
+        TaskDTO t2 = taskService.findOne(t1.getUuid());
+        taskService.unmarkPostoned();
+        assertEquals("Check unmarked postponed status method", t2.getTitle());
+        assert (t2.getStatus() == TaskState.RESTORED.toString());
+        assertEquals("null", t2.getPostponedat());
+        
+     }
+    
 
     @EnableJpaRepositories
     @Configuration
